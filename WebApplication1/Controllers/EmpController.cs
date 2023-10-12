ab1cfca467abb61f8d28f00cc73ab545fd927159
SabaFadhl <sabafadl98@gmai.com>
2023-10-12 21:05:51 +0300

    feat: add controller

---
+++ WebApplication1/Controllers/EmpController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string print()
+        {
+            return "Hello world";
+        }
+        public string getID(int id)
+        {
+            return "ID = "+ id;
+        }
+        public string getAge(int a)
+        {
+            return "your age is " + a;
+        }
+        public int sum(int a , int b)
+        {
+            return a + b;
+        }
+    }
+}
