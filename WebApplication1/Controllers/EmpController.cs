aa30c88c475f59f20ae8fe5d109e7487a7a26561
SabaFadhl <sabafadl98@gmai.com>
2023-10-16 23:41:11 +0300

    feat: temp data

--- WebApplication1/Controllers/EmpController.cs
+++ WebApplication1/Controllers/EmpController.cs
@@ -53,5 +53,32 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Form4()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form4(int n1, int n2)
+        {
+            int sum = n1 + n2;
+            TempData["n1"] = n1.ToString();
+            TempData["n2"] = n2.ToString();
+            TempData["sum"] = sum.ToString();
+            return RedirectToAction("Form5");
+        }
+        [HttpGet]
+        public IActionResult Form5()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Form5")]
+        public IActionResult back()
+        {
+            return RedirectToAction("Form4");
+        }
     }
 }
